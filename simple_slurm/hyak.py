6367687c3678f725d2858e56d1db7c5d7e514c7c
Milin Kodnongbua <mil.millin@hotmail.com>
2023-04-18 19:24:26 -0700

    add squeue and smarter job scheduler

--- simple_slurm/hyak.py
+++ simple_slurm/hyak.py
@@ -1,6 +1,8 @@
-from typing import Dict, Any, NamedTuple, Optional
+from typing import Dict, Any, NamedTuple, Optional, Union
 import subprocess
 import math
+from datetime import datetime, timedelta
+from queue import PriorityQueue
 
 ACCOUNT_SCORE = {"cse": 1.0, "realitylab": 1.0, "stf": 1.0}
 
@@ -167,6 +169,9 @@ class Partition(NamedTuple):
             )
         return self
 
+    def _key(self) -> tuple[str, str]:
+        return (self.account, self.partition)
+
 
 def parse_hyakalloc() -> list[Partition]:
     """
@@ -222,6 +227,85 @@ def parse_hyakalloc() -> list[Partition]:
         )
 
     return partitions
+
+
+class QueueEntry(NamedTuple):
+    account: str
+    partition: str
+    submit_time: datetime
+    time_limit: timedelta
+    end_time: Optional[datetime]
+    state: str
+    resources: Resources
+
+
+def parse_time_limit(time_limit: str) -> timedelta:
+    d = 0
+    dhms = time_limit.split("-")
+    if len(dhms) == 2:
+        d, hms = dhms
+    elif len(dhms) == 1:
+        hms = dhms[0]
+    else:
+        raise ValueError(f"{time_limit} is not a valid time_limit string")
+    h = 0
+    m = 0
+    s = 0
+    hms = hms.split(":")
+    s = hms[-1]
+    if len(hms) > 1:
+        m = hms[-2]
+    if len(hms) > 2:
+        h = hms[-3]
+    return timedelta(days=int(d), hours=int(h), minutes=int(m), seconds=int(s))
+
+
+def parse_tres(tres: str) -> Resources:
+    res = tres.split(",")
+    res_dict: dict[str, str] = {k: v for k, v in map(lambda x: x.split("="), res)}
+    mem = res_dict.get("mem", "0G")
+    mem = int(mem[:-1]) if mem[-1] == "G" else 0
+    return Resources(
+        cpus=int(res_dict.get("cpu", 0)),
+        memory=mem,
+        gpus=int(res_dict.get("gres/gpu", 0)),
+    )
+
+
+def parse_squeue(accounts: Optional[list[str]]) -> list[QueueEntry]:
+    acc = ""
+    if accounts is not None:
+        acc = " -A " + ",".join(accounts)
+    output = call_function(
+        f"squeue{acc} -h -t PD,R -O Account,Partition,SubmitTime,TimeLimit,EndTime,StateCompact,tres-alloc:70"
+    )
+    entries = output.split("\n")
+    qentries = []
+    for entry in entries:
+        (
+            account,
+            partition,
+            submit_time,
+            time_limit,
+            end_time,
+            state,
+            tres,
+        ) = entry.split()
+
+        qentries.append(
+            QueueEntry(
+                account=account,
+                partition=partition,
+                submit_time=datetime.strptime(submit_time, "%Y-%m-%dT%H:%M:%S"),
+                time_limit=parse_time_limit(time_limit),
+                end_time=datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S")
+                if end_time != "N/A"
+                else None,
+                state=state,
+                resources=parse_tres(tres),
+            )
+        )
+    return qentries
 
 
 def compute_score(partition: Partition, constraint: Constraint = Constraint()) -> float:
@@ -317,60 +401,119 @@ def find_best_allocation(constraint: Constraint = Constraint()) -> Allocation:
     return _find_best_allocation(partitions, constraint)
 
 
-PARTITION_TIME_SCALE = {
-    "gpu-a100": 0.5,  # a100 is roughly twice as fast
-    "gpu-a40": 1.0,
-    "gpu-2080ti": 1.2,
-    "gpu-rtx6k": 1.2,
-}
+class _Event(NamedTuple):
+    time: datetime
+    alloc: Allocation
 
-TIME_UNTIL_FREE = 24  # assume currently used gpu will be free in 24 hours
+
+class _PendingTask(NamedTuple):
+    time: datetime
+    constraint: Constraint
+    time_limit: timedelta
 
 
 def find_multiple_allocations(
-    n: int, estimated_hours: float, constraint: Constraint = Constraint()
+    n: int,
+    estimated_time_limit: Union[
+        float, timedelta, dict[str, float], dict[str, timedelta]
+    ],
+    constraint: Constraint = Constraint(),
 ) -> list[Allocation]:
     partitions = parse_hyakalloc()
 
-    EVENT = tuple[float, Allocation]
-    # list of time and which allocations will be free
-    events: list[EVENT] = []
-    for partition in partitions:
-        time_scale = PARTITION_TIME_SCALE.get(partition.partition, 1.5)
-        events.append(
-            (
-                time_scale * TIME_UNTIL_FREE,
-                Allocation(
-                    partition.account, partition.partition, partition.stat.used, -1.0
-                ),
+    partition_map = {p._key(): p for p in partitions}
+
+    pending_queues: dict[tuple[str, str], PriorityQueue] = {
+        p._key(): PriorityQueue() for p in partitions
+    }
+
+    finishing_queue: PriorityQueue = PriorityQueue()
+
+    avail_accounts = list(set([p.account for p in partitions]))
+
+    entries = parse_squeue(avail_accounts)
+
+    for entry in entries:
+        key = (entry.account, entry.partition)
+        if key not in partition_map:
+            continue
+        if entry.state == "R":
+            assert entry.end_time is not None
+            finishing_queue.put(
+                _Event(
+                    entry.end_time,
+                    Allocation(entry.account, entry.partition, entry.resources, -1),
+                )
             )
-        )
+        else:
+            cpus, mem, gpus = entry.resources
+            pending_queues[key].put(
+                _PendingTask(
+                    entry.submit_time, Constraint(cpus, mem, gpus), entry.time_limit
+                )
+            )
 
     # fill up free spaces
-    current_time = 1.0
+    original_time = datetime.now()
+    current_time = datetime.now()
     allocations = []
-    estimated_eta = 0.0
+    estimated_eta = current_time
     while len(allocations) < n:
-        while max(_compute_scores(partitions, constraint)) < TOLERABLE_SCORE:
-            if len(events) == 0:
+        while (
+            max(_compute_scores(list(partition_map.values()), constraint))
+            < TOLERABLE_SCORE
+        ):
+            if finishing_queue.empty():
                 raise ValueError("cannot satisfy constraint")
-            current_time = events[0][0]
-            upto = len(events)
-            for i, (t, alloc) in enumerate(events):
-                if t > current_time:
-                    upto = i
+
+            # fetch next finishing task
+            finished = finishing_queue.get()
+            assert isinstance(finished, _Event)
+            current_time = finished.time
+
+            # release resource
+            key = (finished.alloc.account, finished.alloc.partition)
+            new_partition = partition_map[key].add(finished.alloc)
+
+            # see if pending can take it
+            pending_queue = pending_queues[key]
+            while not pending_queue.empty():
+                next_pending_task = pending_queue.queue[0]
+                assert isinstance(next_pending_task, _PendingTask)
+                if new_partition.free_satisfies(next_pending_task.constraint):
+                    # take it
+                    cpus, mem, gpus, _ = next_pending_task.constraint
+                    alloc = Allocation(key[0], key[1], Resources(cpus, mem, gpus), -1)
+                    new_partition = new_partition.subtract(alloc)
+                    pending_queue.get()
+                    finishing_queue.put(
+                        _Event(current_time + next_pending_task.time_limit, alloc)
+                    )
+                else:
                     break
-                # free up alloc
-                partitions = [partition.add(alloc) for partition in partitions]
-            events = events[upto:]
-        best_alloc = _find_best_allocation(partitions, constraint)
+
+            partition_map[key] = new_partition
+
+        best_alloc = _find_best_allocation(list(partition_map.values()), constraint)
         allocations.append(best_alloc)
-        partitions = [partition.subtract(best_alloc) for partition in partitions]
-        time_scale = PARTITION_TIME_SCALE.get(best_alloc.partition, 1.5)
-        eta = current_time + time_scale * estimated_hours
-        events.append((eta, best_alloc))
-        events = sorted(events, key=lambda x: x[0])
+
+        key = (best_alloc.account, best_alloc.partition)
+        partition_map[key] = partition_map[key].subtract(best_alloc)
+
+        if isinstance(estimated_time_limit, dict):
+            time_limit = estimated_time_limit[best_alloc.partition]
+        else:
+            time_limit = estimated_time_limit
+        if not isinstance(time_limit, timedelta):
+            time_limit = timedelta(hours=time_limit)
+
+        eta = current_time + time_limit
+
+        finishing_queue.put(_Event(eta, best_alloc))
+
         estimated_eta = max(estimated_eta, eta)
 
-    print(f"estimated_eta: {estimated_eta:.2f}")
+    eta_str = estimated_eta.strftime("%Y-%m-%d, %H:%M:%S")
+    eta_delta_str = str(estimated_eta - original_time)
+    print(f"estimated_eta: {eta_str} (in {eta_delta_str})")
     return allocations
